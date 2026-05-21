49b3bfa4fde159e9150afd395cac4d9eca1f7d42
poludovmaksim03-debug <poludovmaksim03@gmail.com>
2026-05-21 07:32:43 +0400

    Update scanner2.js
    Signed-off-by: poludovmaksim03-debug <poludovmaksim03@gmail.com>

--- scanner2.js
+++ scanner2.js
@@ -18,8 +18,8 @@ class DocumentScanner {
             this.stream = await navigator.mediaDevices.getUserMedia({
                 video: {
                     facingMode: 'environment',
-            width: { ideal: 1920 },
-            height: { ideal: 1080 }
+            width: { ideal: 7920 },
+            height: { ideal: 7080 }
                 }
             });
 
